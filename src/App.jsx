3c5b17fc35f0f192ddcd067d84889ee7536f563d
Kais Tarraf <hossamtr184@gmail.com>
2026-03-10 07:25:39 +0300

    Implement SecondVideo section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FirstVideo from "./sections/FirstVideo";
 import Hero from "./sections/Hero";
 import Jason from "./sections/Jason";
 import NavBar from "./sections/Navbar";
+import SecondVideo from "./sections/SecondVideo";
 const App = () => {
 	return (
 		<main>
@@ -9,6 +10,7 @@ const App = () => {
 			<Hero />
 			<FirstVideo />
 			<Jason />
+			<SecondVideo />
 		</main>
 	);
 };
