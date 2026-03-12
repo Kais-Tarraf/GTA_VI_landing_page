3fad518ac159444dcdb704296e762267dc4879c1
Kais Tarraf <hossamtr184@gmail.com>
2026-03-12 08:21:16 +0300

    rebuild project sections from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import Final from "./sections/Final";
-import FirstVideo from "./sections/FirstVideo";
-import Hero from "./sections/Hero";
-import Jason from "./sections/Jason";
-import Lucia from "./sections/Lucia";
-import NavBar from "./sections/Navbar";
-import Postcard from "./sections/Postcard";
-import SecondVideo from "./sections/SecondVideo";
 const App = () => {
-	return (
-		<main>
-			<NavBar />
-			<Hero />
-			<FirstVideo />
-			<Jason />
-			<SecondVideo />
-			<Lucia />
-			<Postcard />
-			<Final />
-		</main>
-	);
+	return <main className="text-3xl text-indigo-500 font-extrabold">Hello</main>;
 };
 
 export default App;
