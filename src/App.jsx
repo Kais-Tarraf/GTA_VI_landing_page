c74faa03acbce8eec851e3403aea8188331dfb5e
Kais Tarraf <hossamtr184@gmail.com>
2026-03-20 08:18:40 +0300

    animate FirstVideo, Implement Jason section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import FirstVideo from "./sections/FirstVideo";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import { ScrollTrigger } from "gsap/all";
+import Jason from "./sections/Jason";
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
 	return (
@@ -10,6 +11,7 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<FirstVideo />
+			<Jason />
 		</main>
 	);
 };
